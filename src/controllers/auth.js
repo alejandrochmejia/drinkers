f0af652f3ae002321e197ddba264d3fe8be125aa
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-09-18 16:06:58 -0400

    Agregando JWT

---
+++ src/controllers/auth.js
@@ -0,0 +1,12 @@
+async function login(req,res) {
+
+}
+
+async function register(req,res) {
+    
+}
+
+export const methods={
+    login,
+    register
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import dotenv from 'dotenv'
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
+import {methods as auth} from './controllers/auth.js'
+import {methods as authorization} from "./middlewares/authorization.js";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 dotenv.config()
@@ -13,16 +16,19 @@ const app = express();
 app.set('port',process.env.PORT)
 app.use(express.json())
 app.listen(app.get('port'),()=>{
-    console.log('Server on port',app.get('port'))
+    console.log('http://localhost:'+app.get('port')+'/')
 })
 app.use(express.static(resolve(__dirname, '..')));
 
 //Rutas
-
 app.get('/',(req,res)=>{
     res.sendFile(resolve(__dirname, '../index.html'))
 })
 
 app.get('/login',(req,res)=>{
     res.sendFile(resolve(__dirname, './routes/login.html'))
+})
+
+app.get('/register',(req,res)=>{
+    res.sendFile(resolve(__dirname, './routes/register.html'))
 })
