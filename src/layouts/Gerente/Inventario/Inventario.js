724fe0912366ba9f34b121c42c8dd80acae850f4
JoSantana0005 <joseasantana05@gmail.com>
2024-09-25 15:24:18 -0400

    Se agrega las tres ventanas modales a la view de inventario

--- src/layouts/Gerente/Inventario/Inventario.js
+++ src/layouts/Gerente/Inventario/Inventario.js
@@ -1,12 +1,29 @@
 let añadir = document.getElementById('Añadir');
 let modificar = document.getElementById('Modificar');
 let eliminar = document.getElementById('Eliminar');
-let modalAñadir = document.getElementById('Agregar')
+let modalAñadir = document.getElementById('dialog--1')
 let button = document.getElementById('submit')
+let eliminarProducto = document.getElementById('dialog--2')
+let buttonEliminar = document.getElementById('delete')
+let modificarproducto = document.getElementById('dialog--3')
+let buttonModificar = document.getElementById('modified')
 
 añadir.addEventListener('click',()=>{
     modalAñadir.showModal();
 })
 button.addEventListener('click',()=>{
     modalAñadir.close()
+})
+eliminar.addEventListener('click',()=>{
+    eliminarProducto.showModal();
+})
+buttonEliminar.addEventListener('click',()=>{
+    eliminarProducto.close();
+})
+
+modificar.addEventListener('click',()=>{
+    modificarproducto.showModal();
+})
+buttonModificar.addEventListener('click',()=>{
+    modificarproducto.close();
 })
