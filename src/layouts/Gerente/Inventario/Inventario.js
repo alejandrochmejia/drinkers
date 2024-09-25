6ab274ccd29bd11898470e3588bc11cfab75e288
JoSantana0005 <joseasantana05@gmail.com>
2024-09-25 14:36:02 -0400

    Se termina la primera ventana modal de la view de inventario que es la de agregar producto

--- src/layouts/Gerente/Inventario/Inventario.js
+++ src/layouts/Gerente/Inventario/Inventario.js
@@ -2,7 +2,11 @@ let añadir = document.getElementById('Añadir');
 let modificar = document.getElementById('Modificar');
 let eliminar = document.getElementById('Eliminar');
 let modalAñadir = document.getElementById('Agregar')
+let button = document.getElementById('submit')
 
 añadir.addEventListener('click',()=>{
     modalAñadir.showModal();
+})
+button.addEventListener('click',()=>{
+    modalAñadir.close()
 })
