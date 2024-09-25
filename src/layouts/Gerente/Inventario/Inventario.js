833c0be6101bbf517e8ad4086bd5ea716cf695da
JoSantana0005 <joseasantana05@gmail.com>
2024-09-25 12:22:24 -0400

    Haciendo la ventana modal para agregar un producto al inventario

---
+++ src/layouts/Gerente/Inventario/Inventario.js
@@ -0,0 +1,8 @@
+let añadir = document.getElementById('Añadir');
+let modificar = document.getElementById('Modificar');
+let eliminar = document.getElementById('Eliminar');
+let modalAñadir = document.getElementById('Agregar')
+
+añadir.addEventListener('click',()=>{
+    modalAñadir.showModal();
+})
