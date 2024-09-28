ca4ec96918fd7acbebe6c9f15197c1892cd65350
JoSantana0005 <joseasantana05@gmail.com>
2024-09-27 22:34:40 -0400

    Se daran comienzo a los cambios en las views

---
+++ src/layouts/Gerente/Proveedor/Proveedor.js
@@ -0,0 +1,6 @@
+let añadir = document.getElementById('Agregar');
+let modalAgregar = document.getElementById('dialog--1');
+//Evento que abre la ventana modal
+añadir.addEventListener('click',()=>{
+    modalAgregar.showModal();
+})
