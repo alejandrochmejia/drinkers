e701160926f80054b2c2121ad06e00645b687bd7
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-10-01 13:34:40 -0400

    Creando Funcionalidad del buscador

--- public/js/admin/mayorista.js
+++ public/js/admin/mayorista.js
@@ -7,3 +7,22 @@ minorista.addEventListener('mouseover',()=>{
 minorista.addEventListener('mouseout',()=>{
     mayorista.style = "color:#FFFFFF; background-color: #001752"
 })
+
+
+let lupa = document.getElementById('Lupa');
+
+lupa.addEventListener('click',()=>{
+    let search = document.getElementById('Destino').value;
+    let tabla = document.querySelector('.Table--Container table tbody');
+    let filas = tabla.querySelectorAll('tr');
+    filas.forEach((fila)=>{
+        let destino = fila.children[2].textContent;
+        if(search === ''){
+            fila.style.display = 'table-row';
+        }else if(destino.includes(search)){
+            fila.style.display = 'table-row';
+        }else{
+            fila.style.display = 'none';
+        }
+    })
+})
--- public/js/admin/minorista.js
+++ public/js/admin/minorista.js
@@ -7,3 +7,21 @@ mayorista.addEventListener('mouseover',()=>{
 mayorista.addEventListener('mouseout',()=>{
     minorista.style = "color:#FFFFFF; background-color: #001752"
 })
+
+let lupa = document.getElementById('Lupa');
+
+lupa.addEventListener('click',()=>{
+    let search = document.getElementById('Destino').value;
+    let tabla = document.querySelector('.Table--Container table tbody');
+    let filas = tabla.querySelectorAll('tr');
+    filas.forEach((fila)=>{
+        let destino = fila.children[2].textContent;
+        if(search === ''){
+            fila.style.display = 'table-row';
+        }else if(destino.includes(search)){
+            fila.style.display = 'table-row';
+        }else{
+            fila.style.display = 'none';
+        }
+    })
+})
