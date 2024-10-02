52068ce16398656d1877de8c8c5a92f1bed19ac2
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-10-02 15:33:42 -0400

    Grafica aplicada a Estadistica

--- public/js/admin/estadistica.js
+++ public/js/admin/estadistica.js
@@ -28,3 +28,43 @@ document.addEventListener('DOMContentLoaded', async () => {
     
     
 })
+
+const ctx = document.getElementById('myChart');
+
+
+const grafica = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: [],
+  datasets: [{
+    label: 'Productos mas vendidos ($)',
+    data: [],
+    borderWidth: 1
+  }]
+},
+options: {
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  },
+  indexAxis: 'y'
+}
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+  const data = await fetch('/api/productos/vendidos', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  const dataJson = await data.json()
+
+  grafica.data.labels = dataJson.map(p => p.nombre);
+  grafica.data.datasets[0].data = dataJson.map(p => p.ingresos);
+
+  grafica.update();
+
+})
