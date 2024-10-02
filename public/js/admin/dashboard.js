19b0a2f00167ad80a8cd5e475b93e7358a140cab
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-10-01 23:29:54 -0400

    omaga

--- public/js/admin/dashboard.js
+++ public/js/admin/dashboard.js
@@ -1,22 +1,24 @@
 const ctx = document.getElementById('myChart');
-  new Chart(ctx, {
+
+const grafica = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Producto 1', 'Producto 2', 'Producto 3', 'Producto 4', 'Producto 5'],
-      datasets: [{
-        label: 'Productos mas vendidos',
-        data: [1, 2, 3, 4, 5],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
+        labels: ['Producto 1', 'Producto 2', 'Producto 3', 'Producto 4', 'Producto 5'],
+  datasets: [{
+    label: 'Productos mas vendidos',
+    data: [1, 2, 3, 4, 5],
+    borderWidth: 1
+  }]
+},
+options: {
+  scales: {
+    y: {
+      beginAtZero: true
     }
-  });
+  }
+}
+});
+
 
 
 function actualizarHora() {
