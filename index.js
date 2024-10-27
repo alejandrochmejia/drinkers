bd66b1c84eaf854016cd4be7a452ef849f0d7efa
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-10-27 19:21:24 -0400

    Erorres

--- index.js
+++ index.js
@@ -106,6 +106,10 @@ app.set('view engine','ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo salió mal!');
+});
 //Rutas
 
 //////////////////
