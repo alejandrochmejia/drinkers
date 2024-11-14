2f584be59b49d5754d9f54a5f4713ac8a0a8871a
JoSantana0005 <joseasantana05@gmail.com>
2024-11-14 15:19:20 -0400

    Arreglos en js del register y las modificaciones en la pagina como tal

--- public/js/auth/register.js
+++ public/js/auth/register.js
@@ -1,8 +1,11 @@
-let Nombre = document.getElementById('Nombre');
-let Apellido = document.getElementById('Apellido');
+let Nombre = document.getElementById('Nombre--persona');
+let Apellido = document.getElementById('Apellido--persona');
+let nacimiento = document.getElementById('Nacimiento');
 let email = document.getElementById('Correo');
 let contraseña = document.getElementById('Password');
-const button = document.getElementsByClassName('Submit--From')[0];
+let user = document.getElementById('Username');
+let direccion = document.getElementById("Direccion");
+const Button = document.getElementById('Cuenta');
 
 // Funcion que valida el correo
 function IsvalidEmail(Email) {
@@ -21,10 +24,22 @@ email.addEventListener('input',()=>{
 })
 // Funcion que valida la contraseña
 contraseña.addEventListener('input',()=>{
-    if (contraseña.value.length == 8) {
+    if (contraseña.value.length == 6) {
         contraseña.style.border = "2px solid #04f"
     } else {
         contraseña.style.border = "2px solid #f00"
+    }
+})
+
+Button.addEventListener('click', (e)=>{
+    e.preventDefault()
+    if (Nombre.value == 0 && Apellido.value == 0 && email.value == 0 && contraseña.value == 0 && nacimiento.value == 0 && user.value == 0 && direccion.value == 0) {
+        alert("Todos los campos esta vacios para hacer la cuenta rellene los campos")
+    } else if(Nombre.value == 0 || Apellido.value == 0 || email.value == 0 || contraseña.value == 0 || nacimiento.value == 0 || user.value == 0 || direccion.value == 0){
+        alert("Alguns campos no esta completos por favor complete los campos faltantes")
+    }else {
+        alert("Se ha creado la cuenta")
+        window.location.href = "../Login/index.html"
     }
 })
 
