e00efda3cf4ba388f3570fa495a8dfd9917bb5e9
alejandrochmejia <alejandrochmejia@gmail.com>
2024-11-16 09:03:39 -0400

    second step modified

--- src/layouts/Landing/pay/main.js
+++ src/layouts/Landing/pay/main.js
@@ -40,3 +40,38 @@ for (let mode of modes) {
         if (selectedImage) {selectedImage.src = '../../../../public/images/icons/checkfill.svg';} // Cambia a la nueva imagen
     });
 }
+
+function setMinDateTime() {
+    const now = new Date();
+    now.setMinutes(now.getMinutes() + 10); // Sumar 10 minutos
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Meses empiezan en 0
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+
+    // Formato para input datetime-local: "YYYY-MM-DDTHH:MM"
+    const dateTime = `${year}-${month}-${day}T${hours}:${minutes}`;
+    const dateTimeInput = document.getElementById("date-delivery");
+    dateTimeInput.min = dateTime;  // Establecer como mínimo
+    dateTimeInput.value = dateTime; // Establecer como valor predeterminado
+
+    updateDateView(new Date(dateTime)); // Actualizar vista inicial
+}
+
+function updateDateView(selectedDate) {
+    const day = selectedDate.getDate();
+    const month = selectedDate.toLocaleString('es-ES', { month: 'long' });
+    const year = selectedDate.getFullYear();
+
+    document.getElementById("day").textContent = day;
+    document.getElementById("month-year").textContent = `${month.charAt(0).toUpperCase() + month.slice(1)}, ${year}`;
+}
+
+let datetimeinput = document.getElementById("date-delivery");
+datetimeinput.addEventListener("input", function() {
+    const selectedDate = new Date(this.value);
+    updateDateView(selectedDate);
+});
+
+window.onload = setMinDateTime;
