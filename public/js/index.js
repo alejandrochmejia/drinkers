bbe7ca244192663aed792fba988bc2296ca37dfc
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-11-20 10:37:33 -0400

    Agregando PopUp

--- public/js/index.js
+++ public/js/index.js
@@ -122,3 +122,18 @@ async function addProduct(button) {
 
     cargarCarrito();
 }
+
+function AdultQuestionButton(){
+    let content = document.querySelector('body');
+    content.style.overflowY = 'auto'
+    let modal = document.getElementById('modalBackground')
+    modal.style.opacity = '0'
+    setTimeout(()=>{
+        modal.style.display = 'none'
+    },200)
+}
+
+document.addEventListener('DOMContentLoaded',() => {
+    let container = document.getElementById('modalContainer')
+    container.style.opacity = "1"
+})
